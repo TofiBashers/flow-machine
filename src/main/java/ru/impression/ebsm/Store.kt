688fe92c3f9d9be97a9406943d43717bf96fb6d7
ArtemiyDmtrvch <artemiy.dmtrvch@gmail.com>
2019-03-06 17:29:51 +0300

    making it library

---
+++ src/main/java/ru/impression/ebsm/Store.kt
@@ -0,0 +1,14 @@
+package ru.impression.ebsm
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.ReplaySubject
+import java.util.concurrent.ConcurrentHashMap
+
+//internal
+val DISPOSABLES: ConcurrentHashMap<String, CompositeDisposable> = ConcurrentHashMap()
+
+//internal
+val EVENT_SUBJECTS: ConcurrentHashMap<String, BehaviorSubject<Flow.Event>> = ConcurrentHashMap()
+
+internal val ACTION_SUBJECTS: ConcurrentHashMap<String, ReplaySubject<Flow.Action>> = ConcurrentHashMap()
